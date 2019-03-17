8c680dfe6a9bb900b1debbcaf81a96370210280b
beardofdoom <marcin.jusiak@gmail.com>
2019-03-17 19:45:23 +0100

    Work with script.js file

--- js/script.js
+++ js/script.js
@@ -1,6 +1,50 @@
+//Main menu - white background on scroll!!
+const mainNavbar = document.querySelector('.navbar');
+
+function mainNavbarBackground() {
+    const scrollValue = window.scrollY;
+
+    if (scrollValue > 100) {
+        mainNavbar.classList.add('active');
+    } else {
+        mainNavbar.classList.remove('active');
+    }
+}
+document.addEventListener('scroll', mainNavbarBackground);
+
+//Main menu - active section
+function activeMenuSection() {
+
+    //Scroll Value 
+    const scrollValue = window.scrollY;
+
+    // Get nav__link from Desktop menu
+    const navLinks = [...document.querySelectorAll('.nav-item')];
+
+    // Section from Top of the screen (in px)
+    const sectionsFromTop = [
+        document.querySelector('.header').offsetTop,
+        document.querySelector('.about').offsetTop,
+        document.querySelector('.services').offsetTop,
+        document.querySelector('.gallery').offsetTop,
+        document.querySelector('.blog').offsetTop,
+        document.querySelector('.contact').offsetTop
+    ];
+
+    navLinks.forEach((navLink, i) => {
+        if (scrollValue > sectionsFromTop[i] && scrollValue < sectionsFromTop[i + 1]) {
+            navLinks[i].classList.add('active');
+        } else {
+            navLinks[i].classList.remove('active');
+        }
+    });
+
+};
+window.addEventListener('scroll', activeMenuSection);
+
+//Services
 btnsServices = document.querySelectorAll('.btn-services');
 servicesTextContent = [...document.querySelectorAll('.services .textContent')];
-
 
 btnsServices.forEach((btn, index) => {
     btn.addEventListener('click', function () {
@@ -10,3 +54,18 @@ btnsServices.forEach((btn, index) => {
     });
 });
 
+//Gallery
+var $galleryBtn = $(".gallery .btn.btn-view");
+let btnActive = true;
+
+$galleryBtn.on("click", function () {
+    $(".wrapper-hide").slideToggle(400);
+    if (btnActive) {
+        // galleryBtn.textContent = "Show less";
+        $galleryBtn.text('Show less');
+    } else if (!btnActive) {
+        // galleryBtn.textContent = "View more";
+        $galleryBtn.text('View more');
+    }
+    btnActive = !btnActive;
+})
